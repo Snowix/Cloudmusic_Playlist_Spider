b6a346cc8d111bb7abe86ebd221689f5b1bafb31
snow <seanx2015@yahoo.com>
2018-06-01 16:21:03 +0800

    fix bugs

--- user_playlist.py
+++ user_playlist.py
@@ -30,13 +30,15 @@ def play_list(user, url):
     while input('reload?y or n? ') == 'y':
         driver.get(url)
     driver.switch_to.frame('g_iframe')
-    c_num = driver.find_element_by_css_selector('#cHeader > h3 > span')
-    num = int(re.search(r'\d+', c_num.text).group())
-    for n in range(int(input('输入滚动到底部次数：'))):
+    for n in range(int(input('输入滚动到底部次数：(加载完所有创建的歌单)'))):
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")    #滚动到底部
         time.sleep(3)
     page = driver.page_source
+    #print(page)
+    c_num = driver.find_element_by_css_selector('#cHeader > h3 > span')
+    num = int(re.search(r'\d+', c_num.text).group())
     source_parser(user, num, page, driver)
+
 
 def source_parser(user, num, page, driver):
     #re_img_list = re.findall(r'<img src=.+?.jpg', page)
@@ -47,28 +49,38 @@ def source_parser(user, num, page, driver):
     #img_list = img_list[:num]    #歌单封面
     play_list = ['http://music.163.com/#'+url[9:-1] for url in re_play_list][:num]
     title_list = [title[19:-2] for title in re_title_list][:num]
+    print(play_list)
+    input()
+    print(title_list)
     save_url(user, title_list, play_list)
     save_playlist(user, num, driver, title_list, play_list)
 
 def save_playlist(user, num, driver, title_list, play_list):
     for title, url in zip(title_list, play_list):
-        driver.get(url)
-        while input('reload?y or n? ') == 'y':
-            driver.get(url)
-        driver.switch_to.frame('g_iframe')
-        songs_ele = driver.find_elements_by_tag_name('tr')
-        songs_ele.pop(0)
-        l = []
         path = os.path.join(user,title)
-        for song in songs_ele:
-            re_name = song.find_element_by_class_name('txt').text
-            name = re.sub(r'\n.+\n', '', re_name).replace('\n', '')    #去除网易云强制加的字符串
-            l.append(name)
-            df = pd.DataFrame(l, columns=['song'])
-            df.to_csv(path + '.csv')
-        print(title + ' saved!')
+        f_name = path + '.csv'
+        if os.path.exists(f_name):
+            print(title + 'exists')
+        else:
+            driver.get(url)
+            '''
+            while input('reload?y or n? ') == 'y':
+                driver.get(url)
+            '''
+            time.sleep(5)
+            driver.switch_to.frame('g_iframe')
+            songs_ele = driver.find_elements_by_tag_name('tr')
+            songs_ele.pop(0)
+            l = []
+            for song in songs_ele:
+                re_name = song.find_element_by_class_name('txt').text
+                name = re.sub(r'\n.+\n', '', re_name).replace('\n', '')    #去除网易云强制加的字符串
+                l.append(name)
+                df = pd.DataFrame(l, columns=['song'])
+                df.to_csv(f_name)
+            print(title + ' saved!')
         num -= 1
-        print(num + ' left')
+        print(num, ' left')
 
 def save_url(user, title_list, play_list):
     file_name = os.path.join(user,user) + '的歌单列表.csv'
